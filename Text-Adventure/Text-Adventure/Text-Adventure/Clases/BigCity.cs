cc2a61553bf4fdbccfcf59f67db44d792f7c63cc
vcitor emmanuel guerra aguado <wasmon999@hotmail.com>
2019-05-17 14:21:57 -0600

    weas

---
+++ Text-Adventure/Text-Adventure/Text-Adventure/Clases/BigCity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Text_Adventure.Clases
+{
+    class BigCity : Location
+    {
+        Dictionary<string, Building> buildings = new Dictionary<string, Building>();
+        BigCity[] possibleExits;
+        bool playerIsHere;
+        int dropChance;
+        int dayCounter;
+
+        public BigCity(string name, string description, int drop): base (name, description)
+        {
+            this.name = name;
+        }
+    }
+}
