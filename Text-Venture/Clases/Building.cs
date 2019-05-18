332b652e15af613c5d94c4361196f9427c8a89ab
Victor-Guerra <48369708+Victor-Guerra@users.noreply.github.com>
2019-05-17 20:44:22 -0600

    FINALLY REBORN

---
+++ Text-Venture/Clases/Building.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Text_Adventure.Clases
+{
+    class Building : Location
+    {
+        private Enums.ETypeBuilding buildingType;
+        private bool playerIsHere;
+        private Dictionary<string, Object> findables;
+
+        
+    }
+}
