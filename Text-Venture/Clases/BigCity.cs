26fc9a972b4bd862efadaac725baf7c013b52312
vcitor emmanuel guerra aguado <wasmon999@hotmail.com>
2019-05-20 16:14:11 -0600

    furula

--- Text-Venture/Clases/BigCity.cs
+++ Text-Venture/Clases/BigCity.cs
@@ -17,22 +17,6 @@ namespace Text_Venture.Clases
 
         public string FilePath { get => filePath; }
 
-
-        public BigCity(string name, string description, string[] possExits, int level) : base(name, description)
-        {
-            this.possExits = possExits.ToList();
-            dropChance = level;
-            Random random = new Random();
-
-            buildings = new Dictionary<string, Building>()
-            {
-                {"1", new Building((ETypeBuilding)random.Next(1, 9))},
-                {"2", new Building((ETypeBuilding)random.Next(1, 9))},
-                {"3", new Building((ETypeBuilding)random.Next(1, 9))}
-            };
-
-            //hacer que diga los buildings que hay y sus salidas
-        }
         public  BigCity(string name, string description) : base(name, description)
         {
             Random random = new Random();
@@ -46,19 +30,7 @@ namespace Text_Venture.Clases
 
             //hacer que diga los buildings que hay y sus salidas
         }
-        public BigCity(string name, string description, string filePath) : base(name, description, filePath)
-        {
-            Random random = new Random();
-            
-            buildings = new Dictionary<string, Building>()
-            {
-                {"1", new Building((ETypeBuilding)random.Next(1, 9))},
-                {"2", new Building((ETypeBuilding)random.Next(1, 9))},
-                {"3", new Building((ETypeBuilding)random.Next(1, 9))}
-            };
-
-            //hacer que diga los buildings que hay y sus salidas
-        }
+     
         public override string onLook() 
         {
             string outt = "vailable exit highways to ";
