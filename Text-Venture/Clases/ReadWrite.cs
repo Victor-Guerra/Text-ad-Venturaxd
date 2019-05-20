d4a2c434d7f2bb55a2bb6c42db19f214410d62f3
Gerardo Granados Aldaz <gerardogr.al@outlook.com>
2019-05-20 10:36:16 -0600

    BB

--- Text-Venture/Clases/ReadWrite.cs
+++ Text-Venture/Clases/ReadWrite.cs
@@ -210,7 +210,7 @@ namespace Text_Venture.Clases
 
         private void playGame()
         {
-            output.AppendText("Your name is {0}, you have heard rumors that the zombie outbreak has not affected Canada. You are currently in {1}.", )
+            output.AppendText("Your name is {0}, you have heard rumors that the zombie outbreak has not affected Canada. You are currently in {1}.", Player.Name)
         }
 
 
